1c950549ab329adaf51132622bb200a0bf99a69e
adepeju4 <adepeju.kayode@gmail.com>
2021-07-22 15:34:24 +0100

    updates

--- src/Landing.js
+++ src/Landing.js
@@ -1,14 +1,5 @@
-import "./stylesheets/landing.css";
-
 const Landing = () => {
-  return (
-    <>
-      <h3>Food Ordering Application.</h3>
-      <p>Landing page</p>
-      <p> ^_^ </p>
-      <div className="goodluck"></div>
-    </>
-  );
+  return <>Landing page</>;
 };
 
 export default Landing;
