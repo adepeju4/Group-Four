f41c4f1aea83997d6e524cf6533e43c73d2ef21c
Rileey <dejiomoloja@gmail.com>
2021-07-24 21:09:36 +0100

    added a new hamburger jpg and worked on the sign up page, I'm having an issue with the footer

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,5 +1,43 @@
+import style from "./stylesheets/signup.module.css";
+
 const SignUp = () => {
-  return <>^_^ this is the sign up page. happy development.</>;
+  return (
+          <div className={style.container}>
+            <div className={style.first}>
+              <div className={style.image}>
+                <img className={style.img} src="/assets/Hamburger-SignUp.jpg"/>
+                <div className={style.logo}>
+                  <p>foodine</p>
+                </div>
+              </div>
+            </div>
+            <div className={style.second}>
+            <img className={style.restaurant} src="/assets/restaurant.png"/>
+            <img className={style.restaurant2} src="/assets/restaurant.png"/>
+              <div className={style.details}>
+                <h5>CREATE AN ACCOUNT</h5>
+                <div className={style.googlebutton}> 
+                  <button type="button" className={style.btn}>Sign up with google</button>
+                </div>
+                <div className={style.form}>
+                  <form action="/">
+                    <div className={style.section1}>
+                      <label className={style.email}>Your email</label>
+                      <input type="text" id="mail" name="email" placeholder="stuff@gmail.com.."/>
+                    </div>
+                    <div className={style.section2}>  
+                      <label className={style.password}>Your password</label>
+                      <input type="password" id="pword" name="password" placeholder="..."/>
+                    </div>
+                    <div className={style.section3}>  
+                      <button type="button" className={style.btn2}>Sign Up</button>
+                    </div>
+                  </form>    
+                  </div>
+                
+              </div>
+            </div>
+          </div>);
 };
 
 export default SignUp;
