2b43d6b6f18c55b8fcd0bd28b59b117c84617438
adepeju4 <adepeju.kayode@gmail.com>
2021-07-24 21:41:34 +0100

    fix: footer & signup

--- src/SignUp.js
+++ src/SignUp.js
@@ -2,42 +2,58 @@ import style from "./stylesheets/signup.module.css";
 
 const SignUp = () => {
   return (
-          <div className={style.container}>
-            <div className={style.first}>
-              <div className={style.image}>
-                <img className={style.img} src="/assets/Hamburger-SignUp.jpg"/>
-                <div className={style.logo}>
-                  <p>foodine</p>
-                </div>
+    <div className={style.container}>
+      <div className={style.first}>
+        <div className={style.image}>
+          <img className={style.img} src="/assets/Hamburger-SignUp.jpg" />
+          <div className={style.logo}>
+            <p>foodine</p>
+          </div>
+        </div>
+      </div>
+      <div className={style.second}>
+        <img className={style.restaurant} src="/assets/restaurant.png" />
+        <img className={style.restaurant2} src="/assets/restaurant.png" />
+        <div className={style.details}>
+          <h5>CREATE AN ACCOUNT</h5>
+          <div className={style.googlebutton}>
+            <button type="button" className={style.btn}>
+              Sign up with google
+            </button>
+          </div>
+          <div className={style.form}>
+            <form action="/">
+              <div className={style.section1}>
+                <label className={style.email}>Your email</label>
+                <input
+                  className={style.inputEmail}
+                  type="text"
+                  id="mail"
+                  name="email"
+                  placeholder="stuff@gmail.com.."
+                />
               </div>
-            </div>
-            <div className={style.second}>
-            <img className={style.restaurant} src="/assets/restaurant.png"/>
-            <img className={style.restaurant2} src="/assets/restaurant.png"/>
-              <div className={style.details}>
-                <h5>CREATE AN ACCOUNT</h5>
-                <div className={style.googlebutton}> 
-                  <button type="button" className={style.btn}>Sign up with google</button>
-                </div>
-                <div className={style.form}>
-                  <form action="/">
-                    <div className={style.section1}>
-                      <label className={style.email}>Your email</label>
-                      <input type="text" id="mail" name="email" placeholder="stuff@gmail.com.."/>
-                    </div>
-                    <div className={style.section2}>  
-                      <label className={style.password}>Your password</label>
-                      <input type="password" id="pword" name="password" placeholder="..."/>
-                    </div>
-                    <div className={style.section3}>  
-                      <button type="button" className={style.btn2}>Sign Up</button>
-                    </div>
-                  </form>    
-                  </div>
-                
+              <div className={style.section2}>
+                <label className={style.password}>Your password</label>
+                <input
+                  type="password"
+                  id="pword"
+                  name="password"
+                  placeholder="..."
+                  className={style.inputPassword}
+                />
               </div>
-            </div>
-          </div>);
+              <div className={style.section3}>
+                <button type="button" className={style.btn2}>
+                  Sign Up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
