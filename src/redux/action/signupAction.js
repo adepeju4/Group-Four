a835b4cde9f4f0111673d5ad30141a3a3942ffc9
Rileey <dejiomoloja@gmail.com>
2021-08-05 00:06:41 +0100

    minor css and axios modifications

--- src/redux/action/signupAction.js
+++ src/redux/action/signupAction.js
@@ -5,9 +5,7 @@ import { types } from "../type/type";
 export const register = (values) => async (dispatch) => {
     dispatch({type: types.REGISTER_REQUEST});
     try {
-        const {data} = await Axios.post('/api/register', {
-            values,
-          })
+        const {data} = await Axios.post('http://127.0.0.1:8800/api/register', {values})
           dispatch({type: types.REGISTER_SUCCESS, payload: data})
           localStorage.setItem('userToken', JSON.stringify(data));
     } catch (error) {
